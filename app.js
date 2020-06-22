bccb5ae2fca4953b549e03fb971b88496d11f067
Colleen Kingsley <colleen.kingsley89@gmail.com>
2020-06-22 19:36:26 -0400

    Working code

---
+++ app.js
@@ -0,0 +1,42 @@
+module.exports = {
+    poorMovie: {
+        acting: 'bad',
+        plotHoles: 'many',
+        specialEffects: 'poor',
+    },
+    goodMovie: {
+        acting: 'good',
+        plotHoles: 'none to few',
+        specialEffects: 'impressive',
+    },
+
+    calculateMovieRating: function (movie) {
+        let rating = 5
+
+        switch (movie.acting) {
+            case 'good':
+                rating = rating + 1
+                break
+            case 'bad':
+                rating = rating - 1
+            default:
+        }
+        switch (movie.plotHoles) {
+            case 'none to few':
+                rating = rating + 2
+                break
+            case 'many':
+                rating = rating - 2
+            default:
+        }
+        switch (movie.specialEffects) {
+            case 'impressive':
+                rating = rating + 2
+                break
+            case 'poor':
+                rating = rating - 2
+            default:
+        }
+        return rating;
+    }
+}
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,13 +1,18 @@
 const assert = require('assert');
 
+const app = require('../app.js')
 
-describe('Movie Ratings', function (){
-    it('3 reasons a movie is bad results in a 0 and a poor rating', function (){
-        const rating = 10;
-        assert.strictEqual(rating, 0)
+let sharktopus = app.poorMovie
+let jurassicPark = app.goodMovie
+
+describe('Movie Ratings', function () {
+    it('3 reasons a movie is bad results in a poor rating', function () {
+        const rating = app.calculateMovieRating(sharktopus);
+        assert.strictEqual(rating, 0);
     });
-    it('3 reasons a movie is good results in a 10 and a good rating', function () {
-        const rating = 0;
-        assert.strictEqual(rating, 10)
+
+    it('3 reasons a movie is good results in a good rating', function () {
+        const rating = app.calculateMovieRating(jurassicPark);
+        assert.strictEqual(rating, 10);
     })
 })
