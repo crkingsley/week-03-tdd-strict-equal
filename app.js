a5a489a50b7b4250f04128c3711c6b296c843f5b
Johnny Rice <jrice@labact.com>
2020-06-22 22:32:50 -0400

    Apply eslint fixes

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
 module.exports = {
-    poorMovie: {
-        acting: 'bad',
-        plotHoles: 'many',
-        specialEffects: 'poor',
-    },
-    goodMovie: {
-        acting: 'good',
-        plotHoles: 'none to few',
-        specialEffects: 'impressive',
-    },
+  poorMovie: {
+    acting: 'bad',
+    plotHoles: 'many',
+    specialEffects: 'poor',
+  },
+  goodMovie: {
+    acting: 'good',
+    plotHoles: 'none to few',
+    specialEffects: 'impressive',
+  },
 
-    calculateMovieRating: function (movie) {
-        let rating = 5
+  calculateMovieRating: function (movie) {
+    let rating = 5
 
-        switch (movie.acting) {
-            case 'good':
-                rating = rating + 1
-                break
-            case 'bad':
-                rating = rating - 1
-            default:
-        }
-        switch (movie.plotHoles) {
-            case 'none to few':
-                rating = rating + 2
-                break
-            case 'many':
-                rating = rating - 2
-            default:
-        }
-        switch (movie.specialEffects) {
-            case 'impressive':
-                rating = rating + 2
-                break
-            case 'poor':
-                rating = rating - 2
-            default:
-        }
-        return rating;
+    switch (movie.acting) {
+      case 'good':
+        rating = rating + 1
+        break
+      case 'bad':
+        rating = rating - 1
+        break
+      default:
     }
+    switch (movie.plotHoles) {
+      case 'none to few':
+        rating = rating + 2
+        break
+      case 'many':
+        rating = rating - 2
+        break
+      default:
+    }
+    switch (movie.specialEffects) {
+      case 'impressive':
+        rating = rating + 2
+        break
+      case 'poor':
+        rating = rating - 2
+        break
+      default:
+    }
+
+    return rating
+  }
 }
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,4 +1,5 @@
-const assert = require('assert');
+/* eslint-disable no-undef */
+const assert = require('assert')
 
 const app = require('../app.js')
 
@@ -6,13 +7,15 @@ let sharktopus = app.poorMovie
 let jurassicPark = app.goodMovie
 
 describe('Movie Ratings', function () {
-    it('3 reasons a movie is bad results in a poor rating', function () {
-        const rating = app.calculateMovieRating(sharktopus);
-        assert.strictEqual(rating, 0);
-    });
+  it('3 reasons a movie is bad results in a poor rating', function () {
+    const rating = app.calculateMovieRating(sharktopus)
 
-    it('3 reasons a movie is good results in a good rating', function () {
-        const rating = app.calculateMovieRating(jurassicPark);
-        assert.strictEqual(rating, 10);
-    })
+    assert.strictEqual(rating, 0)
+  })
+
+  it('3 reasons a movie is good results in a good rating', function () {
+    const rating = app.calculateMovieRating(jurassicPark)
+
+    assert.strictEqual(rating, 10)
+  })
 })
